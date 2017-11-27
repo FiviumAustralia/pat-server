c797f55bfd380173230985425a153ecfe4f46380
ttrimbonias <t.trimbonias@gmail.com>
2017-11-27 13:18:15 +1100

    Tidy up login servlet

--- src/main/java/fivium/pat/servlets/LoginServlet.java
+++ src/main/java/fivium/pat/servlets/LoginServlet.java
@@ -29,15 +29,15 @@ public class LoginServlet extends HttpServlet {
 
 	private static final String ID_PARAM = "id";
 	private static final String PWORD_PARAM = "pwd";
-	private static final String ACTION_PARAM = "action";
+	private static final String LOGIN_CONTEXT_PARAM = "login_context";
 
-	private static final String MOBILE_APP_LOGIN_ACTION = "ma_login";
-	private static final String CLINICIAN_PORTAL_LOGIN_ACTION = "cp_login";
-	private static final String COMPANY_MANAGEMENT_PORTAL_LOGIN_ACTION = "cm_login";
+	private static final String MOBILE_APP_LOGIN_CONTEXT = "mobile_app_login";
+	private static final String CLINICIAN_PORTAL_LOGIN_CONTEXT = "cp_login";
+	private static final String COMPANY_MANAGEMENT_PORTAL_LOGIN_CONTEXT = "cm_login";
 
 	private static final String AUTHENTICATE_PATIENT_PREPARED_SQL_QUERY = "SELECT p_id, company FROM patient WHERE p_id = ? AND Active = \'Not Active\'";
 	private static final String GET_TERMS_AND_CONDITIONS_SQL_QUERY = "SELECT Terms_and_Conditions, Permissions FROM company WHERE Company_Name = ?";
-	private static final String AUTHENTICATE_PATIENT_PREPARED_SQL_QUERY_UPDATE_TOKEN_SQL_QUERY = "UPDATE patient SET Active=?,Token=? where p_id= ?;";
+	private static final String AUTHENTICATE_PATIENT_PREPARED_SQL_QUERY_UPDATE_TOKEN_SQL_QUERY = "UPDATE patient SET Active= \'Active\', Token = ? where p_id= ?;";
 
 	private static final String AUTHENTICATE_CLINICIAN_PREPARED_SQL_QUERY = "SELECT Firstname, Lastname, Password, Role FROM clinicians WHERE Email = ?";
 	private static final String AUTHENTICATE_CLINICIAN_PREPARED_SQL_QUERY_UPDATE_TOKEN = "UPDATE clinicians SET Token=? where Email= ?;";
@@ -59,27 +59,27 @@ public class LoginServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		// extract out expected params from the request
-		String action = request.getParameter(ID_PARAM);
-		String id = request.getParameter(PWORD_PARAM);
-		String password = request.getParameter(ACTION_PARAM);
+		String loginContext = request.getParameter(LOGIN_CONTEXT_PARAM);
+		String id = request.getParameter(ID_PARAM);
+		String password = request.getParameter(PWORD_PARAM);
 
-		if (!isValidAction(action)) {
-			RnsUtils.set400Reponse(response, action + " login action is not supported.");
+		if (!isValidLoginContext(loginContext)) {
+			RnsUtils.set400Reponse(response, loginContext + " login action is not supported.");
 			return;
 		}
 
 		Map<String, Object> verifyResult = new HashMap<>();
 
-		if (MOBILE_APP_LOGIN_ACTION.equals(action)) {
+		if (MOBILE_APP_LOGIN_CONTEXT.equals(loginContext)) {
 			verifyResult = verifyMobileAppLogin(id);
-		} else if (CLINICIAN_PORTAL_LOGIN_ACTION.equals(action)) {
+		} else if (CLINICIAN_PORTAL_LOGIN_CONTEXT.equals(loginContext)) {
 			verifyResult = verifyClinicianPortalLogin(id, password);
-		} else if (COMPANY_MANAGEMENT_PORTAL_LOGIN_ACTION.equals(action)) {
+		} else if (COMPANY_MANAGEMENT_PORTAL_LOGIN_CONTEXT.equals(loginContext)) {
 			// TODO implement when portal exists!
 			// verifyResult = verifyCompanyManagementPortalLogin(id, password);
-			verifyResult.put("error", action + " login action is not supported.");
+			verifyResult.put("error", loginContext + " login action is not supported.");
 		} else {
-			verifyResult.put("error", action + " login action is not supported.");
+			verifyResult.put("error", loginContext + " login action is not supported.");
 		}
 
 		String jsonResult = new Gson().toJson(verifyResult);
@@ -98,14 +98,14 @@ public class LoginServlet extends HttpServlet {
 		doPost(request, response);
 	}
 
-	private boolean isValidAction(String action) {
+	private boolean isValidLoginContext(String action) {
 
 		if (StringUtils.isBlank(action)) {
 			return false;
 		}
 
-		if (action.equals(MOBILE_APP_LOGIN_ACTION) || action.equals(CLINICIAN_PORTAL_LOGIN_ACTION)
-				|| action.equals(COMPANY_MANAGEMENT_PORTAL_LOGIN_ACTION)) {
+		if (action.equals(MOBILE_APP_LOGIN_CONTEXT) || action.equals(CLINICIAN_PORTAL_LOGIN_CONTEXT)
+				|| action.equals(COMPANY_MANAGEMENT_PORTAL_LOGIN_CONTEXT)) {
 			return true;
 		} else {
 			return false;
@@ -116,83 +116,102 @@ public class LoginServlet extends HttpServlet {
 	private Map<String, Object> verifyMobileAppLogin(String id) {
 
 		logger.trace("Entering verifyMobileAppLogin...");
-		Object[] queryArgs = new Object[] { id };
-		logger.info("Authenticating p_id: " + id);
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		Collection<Map<String, String>> result;
-		Collection<Map<String, String>> termsAndConditions;
+
+		// Initialise result of this method
+		Map<String, Object> patientLoginResultMap = new HashMap<String, Object>();
+
 		try {
-			result = PAT_DAO.executeStatement(AUTHENTICATE_PATIENT_PREPARED_SQL_QUERY, queryArgs);
-			if (result.isEmpty()) {
+
+			// Call auth patient query
+			Collection<Map<String, String>> authenticatePatientResult = PAT_DAO
+					.executeStatement(AUTHENTICATE_PATIENT_PREPARED_SQL_QUERY, new Object[] { id });
+			if (authenticatePatientResult.isEmpty()) {
 				throw new Exception("Empty result when executing AUTHENTICATE_PATIENT_PREPARED_SQL_QUERY");
 			}
-			Iterator<Map<String, String>> iter = result.iterator();
-			Map<String, String> resultRow1 = iter.next();
-			String company = resultRow1.get("company");
-			String token = Jwts.builder().setSubject((String) id).signWith(SignatureAlgorithm.HS512, Constants.JWT_KEY)
-					.compact();
-			termsAndConditions = PAT_DAO.executeStatement(GET_TERMS_AND_CONDITIONS_SQL_QUERY, new Object[] { company });
-			Map<String, String> terms = termsAndConditions.iterator().next();
-			Object[] queryArgs_2 = new Object[] { "Active", token, id };
-			PAT_DAO.executeStatement(AUTHENTICATE_PATIENT_PREPARED_SQL_QUERY_UPDATE_TOKEN_SQL_QUERY, queryArgs_2);
-			logger.info("User authenticated, token generated.");
-			resultMap.put("jwt_token", token);
-			resultMap.put("terms", terms.get("Terms_and_Conditions"));
-			resultMap.put("permissions", terms.get("Permissions"));
-			resultMap.put("company", company);
+
+			// Extract company from auth query result
+			String company = authenticatePatientResult.iterator().next().get("company");
+
+			// Create JWT token for the patient
+			String token = issueJwtToken(id, MOBILE_APP_LOGIN_CONTEXT);
+
+			// Fetch terms and conditions for the patient's company
+			Collection<Map<String, String>> termsAndConditionsResult = PAT_DAO
+					.executeStatement(GET_TERMS_AND_CONDITIONS_SQL_QUERY, new Object[] { company });
+			Map<String, String> terms = termsAndConditionsResult.iterator().next();
+
+			// Update database with patient's token
+			PAT_DAO.executeStatement(AUTHENTICATE_PATIENT_PREPARED_SQL_QUERY_UPDATE_TOKEN_SQL_QUERY,
+					new Object[] { token, id });
+
+			// Populate the result
+			patientLoginResultMap.put("jwt_token", token);
+			patientLoginResultMap.put("terms", terms.get("Terms_and_Conditions"));
+			patientLoginResultMap.put("permissions", terms.get("Permissions"));
+			patientLoginResultMap.put("company", company);
 		} catch (Exception e) {
 			logger.error("Exception occurred trying to authenticate p_id", e);
-			resultMap.put("error", "Supplied user id doesn't exist, please contact your clinician");
+			patientLoginResultMap.put("error", "Supplied user id doesn't exist, please contact your clinician");
 		}
 
-		return resultMap;
+		return patientLoginResultMap;
 
 	}
 
-	private Map<String, Object> verifyClinicianPortalLogin(String id, String p_word) {
-		Object[] queryArgs = new Object[] { id, p_word };
+	private Map<String, Object> verifyClinicianPortalLogin(String id, String requestPassword) {
 
-		Collection<Map<String, String>> result;
-		Collection<Map<String, String>> updateTokenResult;
-		Map<String, Object> resultMap = new HashMap<String, Object>();
+		logger.trace("Entering verifyClinicianPortalLogin...");
+		
+		// Initialise result of this method
+		Map<String, Object> clinicianLoginResultMap = new HashMap<String, Object>();
+
 		try {
-			result = PAT_DAO.executeStatement(AUTHENTICATE_CLINICIAN_PREPARED_SQL_QUERY, queryArgs);
-			if (result.isEmpty()) {
-				resultMap.put("jwt_token", "Invalid Credentials");
-				return resultMap;
+			
+			// call auth clinician query
+			Collection<Map<String, String>> authenticateClinicianResult = PAT_DAO
+					.executeStatement(AUTHENTICATE_CLINICIAN_PREPARED_SQL_QUERY, new Object[] { id, requestPassword });
+			if (authenticateClinicianResult.isEmpty()) {
+				throw new Exception("Empty result when executing AUTHENTICATE_CLINICIAN_PREPARED_SQL_QUERY");
 			}
 
-			Map<String, String> aMap = result.iterator().next();
-			String password = aMap.get("Password");
-			String role = aMap.get("Role");
-			String firstname = aMap.get("Firstname");
-			String lastname = aMap.get("Lastname");
-			
-			if (!BCrypt.checkpw(queryArgs[1].toString(), password)) {
-				resultMap.put("jwt_token", "Invalid Credentials");
-				return resultMap;
+			// Extract the value map (row 0) from the auth clinician query result
+			Map<String, String> authenticateClinicianResultValues = authenticateClinicianResult.iterator().next();
+
+			// Verify the request password matches the returned password
+			if (!BCrypt.checkpw(requestPassword, authenticateClinicianResultValues.get("Password"))) {
+				clinicianLoginResultMap.put("jwt_token", "Invalid Credentials");
+				return clinicianLoginResultMap;
 			}
+
+			// Create a JWT token for the clinician
+			String token = issueJwtToken(id, CLINICIAN_PORTAL_LOGIN_CONTEXT);
 			
-			String token = Jwts.builder().setSubject((String) id)
-					.signWith(SignatureAlgorithm.HS512, LegacyInternalServerUtils.JWT_KEY).compact();
-			resultMap.put("Firstname", firstname);
-			resultMap.put("Lastname", lastname);
-			resultMap.put("jwt_token", token);
-			resultMap.put("Role", role);
-			Object[] queryArgsUpdate = new Object[] { token, id };
-			updateTokenResult = PAT_DAO.executeStatement(AUTHENTICATE_CLINICIAN_PREPARED_SQL_QUERY_UPDATE_TOKEN,
-					queryArgsUpdate);
-			return resultMap;
+			// Update the database with the clinician's JWT token
+			PAT_DAO.executeStatement(AUTHENTICATE_CLINICIAN_PREPARED_SQL_QUERY_UPDATE_TOKEN,
+					new Object[] { token, id });
+
+			// Populate the result
+			clinicianLoginResultMap.put("Firstname", authenticateClinicianResultValues.get("Firstname"));
+			clinicianLoginResultMap.put("Lastname", authenticateClinicianResultValues.get("Lastname"));
+			clinicianLoginResultMap.put("Role", authenticateClinicianResultValues.get("Role"));
+			clinicianLoginResultMap.put("jwt_token", token);
 
 		} catch (Exception e) {
 			logger.error("unexpected error occured", e);
-			resultMap.put("jwt_token", "Invalid Credentials");
-			return resultMap;
+			clinicianLoginResultMap.put("jwt_token", "Invalid Credentials");
 		}
+		
+		return clinicianLoginResultMap;
 	}
 
 	private Map<String, Object> verifyCompanyManagementPortalLogin(String id, String password) {
 		return null;
+	}
+
+	private String issueJwtToken(String id, String context) {
+		String token = Jwts.builder().setSubject((String) id).claim(LOGIN_CONTEXT_PARAM, context)
+				.signWith(SignatureAlgorithm.HS512, Constants.JWT_KEY).compact();
+		return token;
 	}
 
 }
