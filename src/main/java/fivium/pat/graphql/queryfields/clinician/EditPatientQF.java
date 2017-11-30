5a60e0c29df09fae4ecaaef64cb17eb2b5f4b609
ttrimbonias <t.trimbonias@gmail.com>
2017-12-01 10:42:23 +1100

    Fix EditPatientQF

--- src/main/java/fivium/pat/graphql/queryfields/clinician/EditPatientQF.java
+++ src/main/java/fivium/pat/graphql/queryfields/clinician/EditPatientQF.java
@@ -14,7 +14,6 @@ import org.apache.commons.logging.LogFactory;
 
 import fivium.pat.graphql.PAT_BaseQF;
 import fivium.pat.utils.PAT_DAO;
-import graphql.GraphQLException;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.GraphQLArgument;
 import graphql.schema.GraphQLObjectType;
@@ -23,16 +22,11 @@ public class EditPatientQF extends PAT_BaseQF {
 
 	private static final String UPDATE_PATIENT_PREPARED_SQL_QUERY = "UPDATE pat.patient_details SET first_name = ?, last_name = ?, mrn = ?, dob = ?, contact = ?, address = ?, email = ?, next_of_kin_relationship = ?, next_of_kin_first_name = ?, next_of_kin_last_name = ?, next_of_kin_contact_number = ? WHERE study_id = ? ";
 	private static Log logger = LogFactory.getLog(EditPatientQF.class);
-	
+
 	@Override
 	protected GraphQLObjectType defineField() {
-		return newObject()
-			    .name("EditPatient")
-			    .description("Makes a request to the intermediate server to edit patient")
-			    .field(newFieldDefinition()
-			    		.name("result")
-			            .type(GraphQLString))		    
-			   .build();	
+		return newObject().name("EditPatient").description("Makes a request to the intermediate server to edit patient")
+				.field(newFieldDefinition().name("result").type(GraphQLString)).build();
 	}
 
 	@Override
@@ -49,46 +43,32 @@ public class EditPatientQF extends PAT_BaseQF {
 				new GraphQLArgument("patient_next_of_kin_relationship", GraphQLString),
 				new GraphQLArgument("patient_next_of_kin_first_name", GraphQLString),
 				new GraphQLArgument("patient_next_of_kin_last_name", GraphQLString),
-				new GraphQLArgument("patient_next_of_kin_contact_number", GraphQLString)
-				);
+				new GraphQLArgument("patient_next_of_kin_contact_number", GraphQLString));
 	}
 
 	@Override
-	protected Object fetchData(DataFetchingEnvironment environment)  {
-		
+	protected Object fetchData(DataFetchingEnvironment environment) {
+
 		Map<String, String> resultMap = new HashMap<String, String>();
 
 		try {
-			//1.  ADD NEW PATIENT DETAILS TO INTERNAL BACKEND DATABASE
-			Object[] queryArgs = new Object[] {
-					environment.getArgument("patient_first_name"),
-					environment.getArgument("patient_last_name"),
-					environment.getArgument("patient_mrn"),
-					environment.getArgument("patient_dob"),
-					environment.getArgument("patient_contact"),
-					environment.getArgument("patient_address"),
-					environment.getArgument("patient_email"),
+			Object[] queryArgs = new Object[] { environment.getArgument("patient_first_name"),
+					environment.getArgument("patient_last_name"), environment.getArgument("patient_mrn"),
+					environment.getArgument("patient_dob"), environment.getArgument("patient_contact"),
+					environment.getArgument("patient_address"), environment.getArgument("patient_email"),
 					environment.getArgument("patient_next_of_kin_relationship"),
 					environment.getArgument("patient_next_of_kin_first_name"),
 					environment.getArgument("patient_next_of_kin_last_name"),
 					environment.getArgument("patient_next_of_kin_contact_number"),
-					environment.getArgument("patient_study_id")};
-
-			try {
-				PAT_DAO.executeStatement(UPDATE_PATIENT_PREPARED_SQL_QUERY, queryArgs);
-			} catch (Exception e) {
-				logger.error("Unexpected error occured", e);
-				e.printStackTrace();
-				throw new GraphQLException("Unexpected execution error", e);
-			}
+					environment.getArgument("patient_study_id") };
+			PAT_DAO.executeStatement(UPDATE_PATIENT_PREPARED_SQL_QUERY, queryArgs);
+			resultMap.put("result", "Patient details sucesfully updated.");
 		} catch (Exception ex) {
 			logger.error("Unexpected error occured", ex);
-			resultMap.put("result", ex.getMessage());
+			resultMap.put("result", "Erorr occured updating patient details");
 		}
-		
+
 		return resultMap;
 	}
-	
-
 
 }
