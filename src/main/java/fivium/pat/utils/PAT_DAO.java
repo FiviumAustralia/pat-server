4f989a4f7b14dade1db7759cab3d33e68a997336
ttrimbonias <t.trimbonias@gmail.com>
2017-12-04 12:15:44 +1100

    Return number of rows affected for mutate calls

--- src/main/java/fivium/pat/utils/PAT_DAO.java
+++ src/main/java/fivium/pat/utils/PAT_DAO.java
@@ -21,16 +21,90 @@ public class PAT_DAO {
 	private static Connection connection;
 	private static Log logger = LogFactory.getLog(PAT_DAO.class);
 
+	private enum QueryType {
+		FETCH, MUTATE
+	}
+
 	private static Connection getConnection() throws SQLException, ClassNotFoundException {
 		if (connection == null) {
 			Class.forName("org.mariadb.jdbc.Driver");
 			// 2147483 seconds ~ 24 days (maximum for mysql run on windows)
-			connection = DriverManager.getConnection(
-					"jdbc:mariadb://localhost:3306/pat?user=root&password=root&sessionVariables=wait_timeout=2147483");
+			connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/pat?user=root&password=root&sessionVariables=wait_timeout=2147483");
 		}
 		return connection;
 	}
 
+	private static Object executeStatement(String sql, Object[] args, QueryType queryType)
+			throws SQLException, ClassNotFoundException {
+		
+		PreparedStatement preparedStatement = getConnection().prepareStatement(sql);
+		// insert args
+		if (args != null) {
+			for (int i = 0; i < args.length; i++) {
+				preparedStatement.setObject(i + 1, args[i]);
+			}
+		}
+
+		switch (queryType) {
+		case FETCH:
+			return preparedStatement.executeQuery();
+		case MUTATE:
+			return preparedStatement.executeUpdate();
+		default:
+			return null;
+		}
+
+	}
+
+	public static Collection<Map<String, String>> executeFetchStatement(String sql, Object[] args)
+			throws SQLException, ClassNotFoundException {
+
+		Collection<Map<String, String>> returnData = new HashSet<Map<String, String>>();
+		ResultSet resultSet = (ResultSet) executeStatement(sql, args, QueryType.FETCH);
+		ResultSetMetaData md = resultSet.getMetaData();
+		int columns = md.getColumnCount();
+		while (resultSet.next()) {
+			HashMap<String, String> row = new HashMap<String, String>(columns);
+			for (int i = 1; i <= columns; ++i) {
+				row.put(md.getColumnName(i), String.valueOf(resultSet.getObject(i)));
+			}
+			returnData.add(row);
+		}
+		return returnData;
+	}
+
+	public static int executeMutateStatement(String sql, Object[] args)
+			throws SQLException, ClassNotFoundException {
+
+		 return (int) executeStatement(sql, args, QueryType.MUTATE);
+		
+	}
+
+	public static void executeSingleSQLStatementInBulk(String sql, Map<String, List<Object>> dataToBeStored) {
+		logger.info("Entering executeBulkStatements...");
+		try {
+			Connection connection = getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql);
+			Set<String> dataSet = dataToBeStored.keySet();
+			for (String x : dataSet) {
+				List<Object> aList = dataToBeStored.get(x);
+				for (int i = 0; i < aList.size(); i++) {
+					statement.setString(i + 1, aList.get(i).toString());
+				}
+				statement.addBatch();
+			}
+			statement.executeBatch(); // Execute every 1000 items.
+		} catch (ClassNotFoundException | SQLException e) {
+			logger.error("ClassNotFoundException | SQLException occured storing the data into the database... " + e);
+		} catch (Exception e) {
+			logger.error("An Exception occured storing the data into the database... " + e);
+		}
+	}
+
+	@Deprecated
+	/**
+	 * Deprecated. - Use executeFetchStatement() or executeMutateStatement() instead.
+	 */
 	public static Collection<Map<String, String>> executeStatement(String sql, Object[] args)
 			throws SQLException, ClassNotFoundException {
 		Collection<Map<String, String>> returnData = new HashSet<Map<String, String>>();
@@ -53,28 +127,6 @@ public class PAT_DAO {
 			returnData.add(row);
 		}
 		return returnData;
-	}
-
-
-	public static void executeSingleSQLStatementInBulk(String sql, Map<String, List<Object>> dataToBeStored) {
-		logger.info("Entering executeBulkStatements...");
-		try {
-			Connection connection = getConnection();
-			PreparedStatement statement = connection.prepareStatement(sql);
-			Set<String> dataSet = dataToBeStored.keySet();
-			for (String x : dataSet) {
-				List<Object> aList = dataToBeStored.get(x);
-				for(int i = 0; i<aList.size();i++) {
-					statement.setString(i+1, aList.get(i).toString());
-				}
-				statement.addBatch();
-			}
-				statement.executeBatch(); // Execute every 1000 items.
-		} catch (ClassNotFoundException | SQLException e) {
-			logger.error("ClassNotFoundException | SQLException occured storing the data into the database... " + e);
-		} catch (Exception e) {
-			logger.error("An Exception occured storing the data into the database... " + e);
-		}
 	}
 	
 }
